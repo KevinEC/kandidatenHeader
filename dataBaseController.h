9e5f2037b7d107b0195107cb5cc1526865f68260
Hilm <hilmautanefternamn@gmail.com>
2019-03-19 11:04:52 +0100

    clean up DBC

--- dataBaseController.h
+++ dataBaseController.h
@@ -13,13 +13,14 @@ class dataBaseController
 {
 public:
 	dataBaseController();
-	dataBaseController(std::string fileLocation, std::string inFileType, std::string filename);
+	dataBaseController(std::string fileLocation, std::string inFileType, std::string url);
 	~dataBaseController();
 
 	std::string fileType;
 	std::string fileLocation;
 	ci::XmlTree * tree;
-	ci::XmlTree* establishConnection(std::string url); //
 
+	ci::XmlTree* establishConnection(std::string url); //
+	
 };
 
