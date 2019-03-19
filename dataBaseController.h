0309d116931f7c1923cab1c48bc9a075c50ed308
Christian Adamsson <christian.liu171@gmail.com>
2019-03-19 20:08:06 +0100

    updating files locally

--- dataBaseController.h
+++ dataBaseController.h
@@ -3,11 +3,13 @@
 #include "cinder/DataSource.h"
 #include "cinder/app/App.h"
 #include "cinder/Log.h"
+#include <string>
+#include <vector>
 
 using namespace ci;
 using namespace ci::app;
 
-#include <string>
+
 
 class dataBaseController
 {
@@ -15,16 +17,15 @@ public:
 	dataBaseController();
 	dataBaseController(std::string fileLocation, std::string inFileType, std::string url);
 	~dataBaseController();
-	
 
 	std::string fileType;
 	std::string fileLocation;
+
 	ci::XmlTree * tree;
-
-	ci::XmlTree* establishConnection(std::string url); 
-
-
-	ci::XmlTree* establishConnection(std::string url); //
-	
+	ci::XmlTree * establishConnection(std::string url);
+	void extractCategories(std::vector <std::string > & categories);
+	void extractTitles(std::vector<std::string> & titles);
+	void extractBodies(std::vector<std::string> & bodyText);
+	void extractImgPaths(std::vector<std::string> & imgPath);
+	void extractCardCats(std::vector<std::string> & cardCategory); // needs extension to extract ALL categories of a card 
 };
-
