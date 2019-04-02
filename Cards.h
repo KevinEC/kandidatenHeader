10a4e471091af1084aa0b1eb232ff9d0dd32217a
Kevin Evans <k.evans.c@gmail.com>
2019-04-02 11:58:06 +0200

    categories are now seemingly sorted correctly. need to call mouseEvents for every instance of Cards still

--- Cards.h
+++ Cards.h
@@ -15,16 +15,19 @@ public:
 	Cards();
 	Cards(string categorie);
 	Cards(vector<pair<string, string>>* titles, vector<pair<string, string>>* bodyText);
-	Cards(vector<pair<string, string>>* titles, vector<pair<string, string>>* bodyText, vector<pair<string, string>>* categories, vector<pair<string, string>>* cardCat, bool createAll);
 	~Cards();
-	void sort();
-	void categorize(vector< pair<string, Cards*> >& sortedCards, vector<pair<string, string>>* cardCat,vector<pair<string, string>>* titles, vector<pair<string, string>>* bodyText);
+	vector<pair<string, Cards*>> categorize(vector<pair<string, string>>* titles, vector<pair<string, string>>* bodyText, vector<string>* categories, vector<vector<string>>* cardCat);
+	vector<pair<string, Cards*>>* sort(vector< pair<string, Cards*> >* sortedCards, vector<vector<string>>* cardCat,vector<pair<string, string >>* titles, vector<pair<string, string >>* bodyText);
 	void collision();
 	void setPath(Card &inst);
 	void search();
 
+	//Help functions
+	void addCard(Card *card);
 	void renderCards();
+	bool findCat(vector<string>::iterator first, vector<string>::iterator last, string searched);
 
+	// Mouse & Touch
 	void	mouseDrag(MouseEvent event) ;
 	void	mouseDown(MouseEvent event) ;
 	void	mouseUp(MouseEvent event) ;
@@ -32,13 +35,14 @@ public:
 	void	touchesMoved(TouchEvent event) ;
 	void	touchesEnded(TouchEvent event) ;
 
-	vector<Card*> allcards;
 
+	vector<Card*> allcards;
+	string categorie;
+	bool render;
+
+	// this should be removed
 	Card testkort;
 	Card rectKort;
-
-	bool loaded;
-	string categorie;
 	//hash_map<string, card[]> categories;'
 
 };
