254eb9105078a5e6e31abe5cd1c70a660ded1835
Hilm <hilmautanefternamn@gmail.com>
2019-04-17 14:59:31 +0200

    story mode impl start, halfscreen and moving cards

--- Story.h
+++ Story.h
@@ -1,12 +1,27 @@
 #pragma once
 #include "Cards.h"
 #include "Transform.h"
+
+#include "bluecadet/core/BaseApp.h"
+#include "bluecadet/views/TouchView.h"
+#include "cinder/Signals.h"
+
+using namespace bluecadet::core;
+using namespace bluecadet::views;
+using namespace bluecadet::touch;
+
 class Story
 {
 public:
 	Story(); //default constructor
 	Story(Cards cards);
 	~Story();
+
+    void	handleTouchBegan(const bluecadet::touch::TouchEvent& touchEvent);
+    void	handleTouchMoved(const bluecadet::touch::TouchEvent& touchEvent);
+    void	handleTouchEnded(const bluecadet::touch::TouchEvent& touchEvent);
+
+    bluecadet::views::TouchViewRef storyView;
 
 private:
 	Cards storyCards = Cards();
