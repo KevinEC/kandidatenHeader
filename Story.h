97953576133700d26df5219d1a544f54ca76fb13
Hilm <hilmautanefternamn@gmail.com>
2019-05-10 10:59:22 +0200

    clean up story constructor and failed 3k times with timing moveToFront and moveToBack

--- Story.h
+++ Story.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "Cards.h"
 #include "Transform.h"
+#include "timercpp.h"
 
 #include "bluecadet/core/BaseApp.h"
 #include "bluecadet/views/TouchView.h"
 #include "bluecadet/views/TextView.h"
 #include "bluecadet/views/ImageView.h"
 #include "cinder/Signals.h"
+#include "cinder/Timer.h"
+#include "cinder/Timeline.h"
 
 using namespace bluecadet::core;
 using namespace bluecadet::views;
@@ -19,12 +22,22 @@ public:
 	Story(Cards cards);
 	~Story();
 
+    void setUpCard(TouchViewRef view, int *offset);
+    void setUpHeader(StrokedRoundedRectViewRef view);
+    void setUpBody(StrokedRoundedRectViewRef view);
+    void setUpImage(StrokedRoundedRectViewRef view);
+
 	vector<pair<string, Cards*>> sort(vector<string>* titles, vector<pair<string, string>>* bodies, vector<string>* imgPaths);
+    
+    void handleTouchesMoved(const bluecadet::touch::TouchEvent& touchEvent);
 
     bluecadet::views::TouchViewRef storyView;
+    
+    Timer t;
+    Timers timer;
 
 private:
-	Cards storyCards = Cards();
-	Transform transform = Transform();
+	Cards storyCards;
+	Transform transform;
 };
 
